2e4019bc1302de56c0a05afb38b9adb288467b9f
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2020-09-04 12:34:13 +0100

    S

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {MapView} from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      region : {
+        latitude : 8.6753,
+        longitude : 9.0920,
+        latitudeDelta : 0.992,
+        longitudeDelta : 0.0421
+      }
+    }
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+  
+  
+        <MapView
+        initialRegion = {this.state.region}
+        rotateEnabled = {false}
+        style ={{
+  
+          flex :1
+        }}
+  
+  
+  
+        
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
