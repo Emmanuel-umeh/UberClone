a8bd02131178892d767b18b93f63834ae538fc1d
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-01-13 14:11:51 +0100

    confirm location screen

--- screens/Confirm_Location.js
+++ screens/Confirm_Location.js
@@ -1,49 +1,45 @@
 import React, { Component } from 'react'
-import MapView, {Marker} from 'react-native-maps'
+import MapView, {Marker, Overlay} from 'react-native-maps'
 import {day_styles, night_styles} from "./map_styles/styles"
 
 import { connect } from 'react-redux'
-import { StyleSheet,Image } from 'react-native'
-
-class Confirm_Location extends Component{
-
+import { StyleSheet,Image, View,Text, SafeAreaView } from 'react-native'
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+  } from "react-native-responsive-screen";
+  import * as Location from "expo-location";
+  import marker from '../assets/markers/marker5.png'
+  
+  const latitudeDelta = 0.025
+  const longitudeDelta = 0.025
+  
+class Confirm_Location extends Component {
 
 
     constructor(props){
         super(props)
 
-        this.marker= null
+        this.marker = null
         this.map = null
     }
-
     state = {
-        scrollEnabled : true
+      region: {
+        latitudeDelta,
+        longitudeDelta,
+        latitude: 25.1948475,
+        longitude: 55.2682899
+      }
     }
-    setDestination(coords) {
-        // const { destination } = this.props;
-        // const lat = coords.lat();
-        // const long = coords.lng();
-        // console.log('seperate:', lat, long);
-        console.log('destina:', coords);
-        // this.props
-        //   .dispatch($setDestination(coords.lat(), coords.lng()))
-        //   .catch((error) => this.props.dispatch(Activity.$toast('failure', error.message)));
-    
-        // this.setState((prevState) => ({
-        //   isVisible: !prevState.isVisible,
-        //   destination: destination[0].fullAddress,
-        // }));
-        
-    }
-    setMarkerPosition =()=>{
-        this.setState({
-scrollEnabled : false
-        })
+  
+    onRegionChange = region => {
+      this.setState({
+        region
+      })
     }
 
-    centerCamera =()=>{
-
-        const {latitude, longitude} = this.props.order.region
+    centerCamera= ()=>{
+var {latitude, longitude} = this.props.order.region
         this.map &&
         this.map.animateCamera(
           {
@@ -54,75 +50,75 @@ scrollEnabled : false
             pitch: 20,
             heading: 30,
             altitude: 100,
-            zoom: 19,
+            zoom: 16,
           },
           800
         );
     }
-
-    componentDidMount(){
-        this.centerCamera()
-    }
-    render(){
-
-        return (
-
-
-            <MapView 
-            
-
-            style={{
-                // flex: 1,
-                ...StyleSheet.absoluteFillObject,
-                // zIndex: 0,
-              }}
-
-              scrollEnabled ={this.state.scrollEnabled}
-            followUserLocation={false}
-            initialRegion={this.props.order.region}
-            rotateEnabled={false}
-            onMapReady ={this.centerCamera}
+  
+    render() {
+      const { region } = this.state
+  
+      return (
+        <View style={styles.map}>
+          <MapView
+            style={styles.map}
             customMapStyle = {day_styles}
-            showsUserLocation={false}
-            showsBuildings={false}
-            zoomEnabled={true}
             ref={(map) => {
                 this.map = map;
               }}
-  
-            showsCompass={false}>
 
-
-<Marker
-        ref={(ref) => { this.marker = ref; }}
-        draggable
-        onDragEnd={(t, map, coords) => this.setDestination(coords)}
-        coordinate={{
-            latitude : this.props.order.region.latitude,
-            longitude:this.props.order.region.longitude
-        }}
-        // position={destination}
-        centerOffset={{ x: -18, y: -60 }}
-        anchor={{ x: 0.69, y: 1 }}
-        // pinColor={COLOR.marker}
-
-   
-        // style={{ width: 200, height: 80 }}
-        onDragStart={() => this.setMarkerPosition()}
-      >
-
-<Image
-            source={require('../assets/markers/marker1.png')}
-            style={{width: 60, height: 70 }}
-            resizeMode="contain"
+              onMapReady ={this.centerCamera}
+            initialRegion={this.props.order.region}
+            onRegionChangeComplete={this.onRegionChange}
+            showsUserLocation = {true}
+            showsCompass = {false}
           />
-      </Marker>
-
-            </MapView>
-        )
+          <View style={styles.markerFixed}>
+            <Image style={styles.marker} style={{
+                width : 60
+            }} source={marker} />
+          </View>
+          <SafeAreaView style={styles.footer}>
+            <Text style={styles.region}>{JSON.stringify(region, null, 2)}</Text>
+            
+          </SafeAreaView>
+        </View>
+      )
     }
-}
+  }
+  
+  const styles = StyleSheet.create({
+    map: {
+      flex: 3,
 
+    },
+    markerFixed: {
+      left: wp(50),
+      marginLeft: -24,
+      marginTop: -48,
+      position: 'absolute',
+      top: hp(50)
+    },
+    marker: {
+      height: 48,
+      width: 48
+    },
+    footer: {
+      backgroundColor: "white",
+      borderTopLeftRadius : 50,
+      borderTopRightRadius : 50,
+      bottom: 0,
+      flex : 1,
+      // position: 'absolute',
+      width: '100%'
+    },
+    region: {
+      color: '#fff',
+      lineHeight: 20,
+      margin: 20
+    }
+  })
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
